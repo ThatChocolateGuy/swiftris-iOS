4b7113c58516999bb775467d2dc0198ce73a19ed
ThatChocolateGuy <thatchocolateguy@ThatChocolateGuys-Mac-mini.local>
2018-09-10 12:39:48 -0400

    Files Push

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,28 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by ThatChocolateGuy on 2018-09-06.
+//  Copyright © 2018 ThatChocolateGuy. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
